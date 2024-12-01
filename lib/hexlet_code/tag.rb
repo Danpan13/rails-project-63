a5d74c306c9b0d38b2a407f2bc264e72ad4c61a8
Danil Pankrashin <danil@tremendous.com>
2024-12-01 15:12:02 +0100

    tag

---
+++ lib/hexlet_code/tag.rb
@@ -0,0 +1,15 @@
+module HexletCode
+  module Tag
+    def self.build(name, **attributes)
+      if block_given?
+        "<#{name}#{attributes_string(attributes)}>#{yield}</#{name}>"
+      else
+        "<#{name}#{attributes_string(attributes)} />"
+      end
+    end
+
+    def self.attributes_string(attributes)
+      attributes.map { |key, value| " #{key}='#{value}'" }.join
+    end
+  end
+end
