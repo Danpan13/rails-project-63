d493be4b8c56fc2a3844a647ac40f7e8b042dbfe
Danil Pankrashin <danil@tremendous.com>
2024-12-01 19:13:07 +0100

    html fixture

--- spec/hexlet_code_spec.rb
+++ spec/hexlet_code_spec.rb
@@ -4,9 +4,10 @@ require 'hexlet_code'
 
 RSpec.describe HexletCode do
   let(:user) { Struct.new(:name, :job, :gender, keyword_init: true).new(name: 'rob', gender: "m") }
+  let(:expected_form_output) { File.read('spec/fixtures/expected_form_output.html', encoding: 'UTF-8').strip }
 
   it 'generates an empty form' do
-    expect(HexletCode.form_for(user)).to eq('<form action="#" method="post"></form>')
+    expect(HexletCode.form_for(user)).to eq(expected_form_output)
   end
 
   # it 'generates a form with additional attributes' do
@@ -17,4 +18,3 @@ RSpec.describe HexletCode do
   #   expect(HexletCode.form_for(user, url: '/profile', class: 'hexlet-form')).to eq('<form action="/profile" method="post" class="hexlet-form"></form>')
   # end
 end
-
